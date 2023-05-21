3571cd9750fc7b26d8e7be7b1776bd46cb88bdc9
Isael.R.A <ir91146559@gamil.com>
2023-05-20 23:24:39 -0300

    first commit

---
+++ api_blaze copy.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import telebot
+from time import sleep
+
+
+cont = 0
+#giwhile cont < 1:
+    request_api = requests.get('https://blaze.com/api/roulette_games/recent')
+    response = request_api.json()
+
+    giros = []
+    for item in response:
+        if item['color'] == 0:
+            item['color'] = 'Branco'
+        elif item['color'] == 1:
+            item['color'] = 'Vermelha'
+        else:
+            item['color'] = 'Preta'
+        giros.append(f'{item["roll"]}  {item["color"]}')
+
+    for giro in giros[:4]:
+        print(giro)
+    cont += 1
+    sleep(15)
+    print('-'*20)
---
+++ bot_telegram.py
@@ -0,0 +1,38 @@
+import telebot
+import requests
+import json
+from time import sleep
+
+CHAVE_API = ''
+bot = telebot.TeleBot(CHAVE_API)
+id_grupo = '-1001921724490'
+
+
+@bot.message_handler(commands=['start'])
+def enviar_msg_grupo(mensagem):
+    if mensagem:
+        while True:
+            request_api = requests.get('https://blaze.com/api/roulette_games/recent')
+            response = request_api.json()
+            ultimos_resultados = []
+            for item in response:
+                if item['color'] == 0:
+                    item['color'] = 'Branco'
+                elif item['color'] == 1:
+                    item['color'] = 'Vermelha'
+                else:
+                    item['color'] = 'Preta'
+                ultimos_resultados.append(f'{item["roll"]}  {item["color"]}')
+            sleep(15)    
+            bot.send_message(id_grupo, f"Quatro ultimos resultados{ultimos_resultados[:4]}")
+
+
+def verificar(mensagem):
+    return True
+
+@bot.message_handler(func=verificar)
+def responder(mensagem):
+    texto = '/start'
+    bot.reply_to(mensagem, texto)
+
+bot.polling()
